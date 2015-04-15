00d3192ebe67718d27a8c6c1347ae5525e0237da
Geoff Lawler <glawler@tislabs.com>
2015-04-15 10:17:51 -0700

    empty module - setup.py and nothing else.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name = "rabidsqlr",
+    packages = ["rabidsqrl"],
+    version = "0.0.1",
+    description = "SQLinjection attack tool",
+    author = "Geoff Lawler",
+    author_email = "glawler@tislabs.com",
+    maintainer = "Geoff Lawler",
+    maintainer_email = "glawler@tislabs.com",
+    url = "http://deterlab.net",
+    download_url = None,
+    license = "BSD 3-Clause \"Revised\" License",
+    keywords = ["sqlinjection", "attack", "pentest"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research :: Network Security Researchers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Security",
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Topic :: System :: Networking",
+        ],
+    long_description = """\
+    TBD
+"""
+)
